03374d633e6dc6b77d620d00456eb5409a125563
Tommi-JP <tjpollanen@gmail.com>
2024-06-20 14:59:26 +0300

    added image shaping

--- Cat_dog_NN.py
+++ Cat_dog_NN.py
@@ -1,13 +1,59 @@
-import tensorflow as tf
-#from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import numpy as np
-import matplotlib.pyplot as plt
 from functions import *
 
-# directory where pictures are
+# the directory where the pictures are, can be changed if necessary
 image_dir = "../Cat_Dog_data"
-image_loader(image_dir)
-
+# batch size and image shape variables, can be changed if necessary
 batch_size = 32
 img_shape = 224
 
+
+train_dir = os.path.join(image_dir, "train")
+val_dir = os.path.join(image_dir, "test")
+
+cats_train_dir = os.path.join(train_dir, "cat")
+dogss_train_dir = os.path.join(train_dir, "dog")
+
+cats_val_dir = os.path.join(val_dir, "cat")
+dogs_val_dir = os.path.join(val_dir, "dog")
+
+print(f"total training images cats:{len(os.listdir(cats_train_dir))}")
+print(f"total training images dogs:{len(os.listdir(dogss_train_dir))}")
+
+print(f"total validation images cats:{len(os.listdir(cats_val_dir))}")
+print(f"total validation images dogs:{len(os.listdir(dogs_val_dir))}")
+
+print(f"total training images:{cats_train_dir + dogss_train_dir}")
+print(f"total validation images:{cats_val_dir + dogs_val_dir}")
+
+
+# shaping images for training, can be changed if necessary
+image_gen_train = ImageDataGenerator(
+      rescale=1./255, # rescales the pixel values of the images by dividing them by 255, which normalizes the pixel values to be between 0 and 1
+      rotation_range=30, # randomly rotates the images
+      width_shift_range=0.2, # randomly shifts the images horizontally
+      height_shift_range=0.2, # and same verticaly
+      shear_range=0.2, # applies a shearing transformation with a shear intensity
+      zoom_range=0.2, # randomly zooms into the images
+      horizontal_flip=True, # randomly flips the images horizontally
+      fill_mode='nearest') # determines how newly created pixels are filled in when the image is transformed. 'nearest' means that the nearest pixel value will be used to fill in the gaps.
+
+train_data_gen = image_gen_train.flow_from_directory(batch_size=batch_size,
+                                                     directory=train_dir,
+                                                     shuffle=True,
+                                                     target_size=(img_shape,img_shape),
+                                                     class_mode='binary')
+
+augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+show_image(augmented_images)
+
+
+# shaping images for validiation
+image_gen_val = ImageDataGenerator(rescale=1./255)
+
+val_data_gen = image_gen_val.flow_from_directory(batch_size=batch_size,
+                                                 directory=val_dir,
+                                                 target_size=(img_shape, img_shape),
+                                                 class_mode='binary')
+
+model_compiler()
+model_summary()
--- functions.py
+++ functions.py
@@ -1,25 +1,65 @@
-import sys
 import os
+import tensorflow as tf
+from keras_preprocessing.image import ImageDataGenerator
+import numpy as np
+import matplotlib.pyplot as plt
 
-def image_loader(image_dir):
-    train_dir = os.path.join(image_dir, "train")
-    val_dir = os.path.join(image_dir, "test")
+def show_image(images_arr):
+    """
+    Display a set of images based on user input.
 
-    cats_train_dir = os.path.join(train_dir, "cat")
-    dogss_train_dir = os.path.join(train_dir, "dog")
+    Parameters:
+    images_arr (list): A list of 5 images to be displayed.
 
-    cats_val_dir = os.path.join(val_dir, "cat")
-    dogs_val_dir = os.path.join(val_dir, "dog")
+    Prompts the user to decide whether or not to show the images.
+    """
+    u_input = input("Show images? (Y/N)")
+    if u_input in ["Y", "y", "Yes", "yes"]:
+        fig, axes = plt.subplots(1, 5, figsize=(20,20))
+        axes = axes.flatten()
+        for img, ax in zip(images_arr, axes):
+            ax.imshow(img)
+        plt.tight_layout()
+        plt.show()
 
-    print(f"total training images cats:{len(os.listdir(cats_train_dir))}")
-    print(f"total training images dogs:{len(os.listdir(dogss_train_dir))}")
-
-    print(f"total validation images cats:{len(os.listdir(cats_val_dir))}")
-    print(f"total validation images dogs:{len(os.listdir(dogs_val_dir))}")
-
-    print(f"total training images:{cats_train_dir + dogss_train_dir}")
-    print(f"total validation images:{cats_val_dir + dogs_val_dir}")
+    elif u_input in ["N", "n", "No", "no"]:
+        print("")
+    else:
+        print("Invalid input")
 
 
+def model_compiler():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(224, 224, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+
+        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+
+        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+
+        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(2)
+        ])
+    
+    model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+    
+    return model
 
 
+def model_summary(model):
+    u_input = input("Do you want to see model summary? (Y/N)")
+    if u_input in ["Y", "y", "Yes", "yes"]:
+        model.summary()
+    elif u_input in ["N", "n", "No", "no"]:
+        print("")
+    else:
+        print("Invalid input")
