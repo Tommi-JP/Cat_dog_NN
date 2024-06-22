92f61d5de3fe69d3ce1a97649a269b7aaec48ccb
Tommi-JP <tjpollanen@gmail.com>
2024-06-22 18:55:31 +0300

    compiling, train and validiation

--- Cat_dog_NN.py
+++ Cat_dog_NN.py
@@ -1,59 +1,90 @@
 from functions import *
 
-# the directory where the pictures are, can be changed if necessary
-image_dir = "../Cat_Dog_data"
-# batch size and image shape variables, can be changed if necessary
-batch_size = 32
-img_shape = 224
 
+def main():
+    # the directory where the pictures are, can be changed if necessary
+    image_dir = "../Cat_Dog_data"
+    # batch size and image shape variables, can be changed if necessary
+    batch_size = 32
+    img_shape = 224
+    # epochs
+    epochs = 1
+    # save outputfile
+    file_path_output = "output.txt"
 
-train_dir = os.path.join(image_dir, "train")
-val_dir = os.path.join(image_dir, "test")
+    train_dir = os.path.join(image_dir, "train")
+    val_dir = os.path.join(image_dir, "test")
 
-cats_train_dir = os.path.join(train_dir, "cat")
-dogss_train_dir = os.path.join(train_dir, "dog")
+    cats_train_dir = os.path.join(train_dir, "cat")
+    dogs_train_dir = os.path.join(train_dir, "dog")
 
-cats_val_dir = os.path.join(val_dir, "cat")
-dogs_val_dir = os.path.join(val_dir, "dog")
+    cats_val_dir = os.path.join(val_dir, "cat")
+    dogs_val_dir = os.path.join(val_dir, "dog")
 
-print(f"total training images cats:{len(os.listdir(cats_train_dir))}")
-print(f"total training images dogs:{len(os.listdir(dogss_train_dir))}")
+    total_train = len(os.listdir(cats_train_dir)) + len(os.listdir(dogs_train_dir))
+    total_val = len(os.listdir(cats_val_dir)) + len(os.listdir(dogs_val_dir))
 
-print(f"total validation images cats:{len(os.listdir(cats_val_dir))}")
-print(f"total validation images dogs:{len(os.listdir(dogs_val_dir))}")
+    print(f"Total training images cats: {len(os.listdir(cats_train_dir))}")
+    print(f"Total training images dogs: {len(os.listdir(dogs_train_dir))}")
 
-print(f"total training images:{cats_train_dir + dogss_train_dir}")
-print(f"total validation images:{cats_val_dir + dogs_val_dir}")
+    print(f"Total validation images cats: {len(os.listdir(cats_val_dir))}")
+    print(f"Total validation images dogs: {len(os.listdir(dogs_val_dir))}")
 
+    print(f"Total training images: {total_train}")
+    print(f"Total validation images: {total_val}")
 
-# shaping images for training, can be changed if necessary
-image_gen_train = ImageDataGenerator(
-      rescale=1./255, # rescales the pixel values of the images by dividing them by 255, which normalizes the pixel values to be between 0 and 1
-      rotation_range=30, # randomly rotates the images
-      width_shift_range=0.2, # randomly shifts the images horizontally
-      height_shift_range=0.2, # and same verticaly
-      shear_range=0.2, # applies a shearing transformation with a shear intensity
-      zoom_range=0.2, # randomly zooms into the images
-      horizontal_flip=True, # randomly flips the images horizontally
-      fill_mode='nearest') # determines how newly created pixels are filled in when the image is transformed. 'nearest' means that the nearest pixel value will be used to fill in the gaps.
+    # shaping images for training, can be changed if necessary
+    image_gen_train = ImageDataGenerator(
+        rescale=1./255, # rescales the pixel values of the images by dividing them by 255, which normalizes the pixel values to be between 0 and 1
+        rotation_range=30, # randomly rotates the images
+        width_shift_range=0.2, # randomly shifts the images horizontally
+        height_shift_range=0.2, # and same vertically
+        shear_range=0.2, # applies a shearing transformation with a shear intensity
+        zoom_range=0.2, # randomly zooms into the images
+        horizontal_flip=True, # randomly flips the images horizontally
+        fill_mode='nearest' # determines how newly created pixels are filled in when the image is transformed. 'nearest' means that the nearest pixel value will be used to fill in the gaps.
+    )
 
-train_data_gen = image_gen_train.flow_from_directory(batch_size=batch_size,
-                                                     directory=train_dir,
-                                                     shuffle=True,
-                                                     target_size=(img_shape,img_shape),
+    train_data_gen = image_gen_train.flow_from_directory(batch_size=batch_size,
+                                                         directory=train_dir,
+                                                         shuffle=True,
+                                                         target_size=(img_shape, img_shape),
+                                                         class_mode='binary')
+
+    augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+    show_image(augmented_images)
+
+    # shaping images for validation
+    image_gen_val = ImageDataGenerator(rescale=1./255)
+
+    val_data_gen = image_gen_val.flow_from_directory(batch_size=batch_size,
+                                                     directory=val_dir,
+                                                     target_size=(img_shape, img_shape),
                                                      class_mode='binary')
 
-augmented_images = [train_data_gen[0][0][0] for i in range(5)]
-show_image(augmented_images)
+    model = model_compiler_summary()
+
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+    u_input = input("Everything looks good? Y/N")
+    if u_input in ["Y", "y", "Yes", "yes"]:
+        print("Training started!")
+    elif u_input in ["N", "n", "No", "no"]:
+        sys.exit()
+    else:
+        print("Invalid input")
+
+    with tf.device('/GPU:0' if tf.config.list_physical_devices('GPU') else '/CPU:0'):
+        history = model.fit(
+            train_data_gen,
+            steps_per_epoch=int(np.ceil(total_train / float(batch_size))),
+            epochs=epochs,
+            validation_data=val_data_gen,
+            validation_steps=int(np.ceil(total_val / float(batch_size)))
+        )
+      
+    statistic(history, epochs, file_path_output)
 
 
-# shaping images for validiation
-image_gen_val = ImageDataGenerator(rescale=1./255)
-
-val_data_gen = image_gen_val.flow_from_directory(batch_size=batch_size,
-                                                 directory=val_dir,
-                                                 target_size=(img_shape, img_shape),
-                                                 class_mode='binary')
-
-model_compiler()
-model_summary()
+if __name__ == "__main__":
+    main()
--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 def show_image(images_arr):
     """
@@ -15,7 +16,7 @@ def show_image(images_arr):
     """
     u_input = input("Show images? (Y/N)")
     if u_input in ["Y", "y", "Yes", "yes"]:
-        fig, axes = plt.subplots(1, 5, figsize=(20,20))
+        fig, axes = plt.subplots(1, 5, figsize=(20, 20))
         axes = axes.flatten()
         for img, ax in zip(images_arr, axes):
             ax.imshow(img)
@@ -27,39 +28,68 @@ def show_image(images_arr):
     else:
         print("Invalid input")
 
+def model_compiler_summary():
+    """
+    Creates and compiles a Keras neural network model, asks the user if they want to save
+    the model summary in .txt file. Values can be changed if necessary.
 
-def model_compiler():
+    Returns:
+        model (tf.keras.Model): Compiled Keras neural network model.
+    """
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(224, 224, 3)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
         tf.keras.layers.MaxPooling2D(2, 2),
 
-        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
 
-        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
 
-        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
 
         tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(512, activation='relu'),
         tf.keras.layers.Dense(2)
-        ])
-    
-    model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-    
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+
+    # ask user if one wants to create summary.txt file
+    u_input = input("Do you want to save model summary? (Y/N)")
+    if u_input in ["Y", "y", "Yes", "yes"]:
+        with open("summary.txt", "w") as file:
+            original_stdout = sys.stdout
+            sys.stdout = file
+            model.summary()
+            sys.stdout = original_stdout
+    elif u_input in ["N", "n", "No", "no"]:
+        print("Summary not saved")
+    else:
+        print("Invalid input")
+
     return model
 
 
-def model_summary(model):
-    u_input = input("Do you want to see model summary? (Y/N)")
-    if u_input in ["Y", "y", "Yes", "yes"]:
-        model.summary()
-    elif u_input in ["N", "n", "No", "no"]:
-        print("")
-    else:
-        print("Invalid input")
+def statistic(history, epochs, file_path_output):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    original_stdout = sys.stdout
+    with open(file_path_output, "w") as f:
+        sys.stdout = f 
+        for epoch in epochs_range:
+            print(f"Epoch: {epoch+1}: Accuracy: {acc[epoch]}, Loss: {loss[epoch]}")
+    
+    sys.stdout = original_stdout
